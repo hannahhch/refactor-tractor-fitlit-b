433de95109b1b3166d8fa1a91341538ddb20bfd5
Fara Akhatova <faraakhatova@gmail.com>
2021-07-27 13:12:19 -0600

    Add notes for bullet point user class

--- src/User.js
+++ src/User.js
@@ -18,3 +18,23 @@ class User {
 }
 
 export default User;
+/*
+
+User class
+[ ] new User(userData);
+[ ]  A User represents a single user
+[ ]  It should have a parameter to take in a userData object
+[ ] Each user holds on to the user properties from the data file
+[ ] Should have a method to:
+[ ] Return a user’s first name only
+
+Dashboard
+Use the scripts.js file to add information to the DOM. This JS file should call methods from your classes to retrieve information. There should not be any DOM manipulation within the User or UserRepository class files.
+
+To develop this dashboard, first choose a user at random - someone with a randomly generated name that speaks to you. On the dashboard for a user:
+
+[ ] Create an info card on the dashboard with all of user’s info on the page
+[ ] Display their first name somewhere prominently on the page to welcome them
+[ ] For a specific user, display how their step goal compares to the average step goal amongst all users (this display should not be hard-coded)
+
+*/
