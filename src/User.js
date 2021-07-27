e7a6fc5a8bc661a8711ed29ad3c92e2cabe464f1
Fara Akhatova <faraakhatova@gmail.com>
2021-07-27 14:52:03 -0600

    Add some refctor test on user repo test

--- src/User.js
+++ src/User.js
@@ -22,8 +22,8 @@ export default User;
 
 User class
 [ ] new User(userData);
-[ ]  A User represents a single user
-[ ]  It should have a parameter to take in a userData object
+[ ] A User represents a single user
+[ ] It should have a parameter to take in a userData object
 [ ] Each user holds on to the user properties from the data file
 [ ] Should have a method to:
 [ ] Return a user’s first name only
