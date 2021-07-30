69acf5c857c364f72fae57fe1be86849be7f4ff4
Fara Akhatova <faraakhatova@gmail.com>
2021-07-30 10:30:07 -0600

    Add refactor to activity class, break down properties of the class to check in individual it block

--- src/Activity.js
+++ src/Activity.js
@@ -1,6 +1,10 @@
 class Activity {
   constructor(activityData) {
-    this.activityData = activityData
+    this.userID = activityData.userID;
+    this.date = activityData.date;
+    this.numSteps = activityData.numSteps;
+    this.minutesActive = activityData.minutesActive;
+    this.flightsOfStairs = activityData.flightsOfStairs;
   }
   getMilesFromStepsByDate(id, date, userRepo) {
     let userStepsByDate = this.activityData.find(data => id === data.userID && date === data.date);
